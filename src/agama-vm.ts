11776c3e64881c97b05f2ed87857df3da8476c81
Ladislav Slezák <lslezak@suse.com>
2024-11-23 21:33:23 +0100

    Set the root password

--- src/agama-vm.ts
+++ src/agama-vm.ts
@@ -14,18 +14,32 @@ program
 program.parse();
 const options = program.opts();
 
+// root password configured for the started Agama instance
+const password = "linux";
+
 let tmpdir;
 try {
   tmpdir = fs.mkdtempSync(path.join(os.tmpdir(), "agama-vm-"));
 
   // create sparse file for the raw disk image
   const disk = path.join(tmpdir, "disk");
-  spawnSync("truncate", ["-s", "50G", disk])
+  spawnSync("truncate", ["-s", "50G", disk]);
+
+  const linux = path.join(tmpdir, "linux");
+  const initrd = path.join(tmpdir, "initrd");
+
+  spawnSync("isoinfo", ["-R", "-i", options.iso, "-x", "/boot/x86_64/loader/linux", ">", linux], {
+    shell: true,
+  });
+
+  spawnSync("isoinfo", ["-R", "-i", options.iso, "-x", "/boot/x86_64/loader/initrd", ">", initrd], {
+    shell: true,
+  });
 
   console.log("Starting Agama VM...\n");
   console.log("Connect to https://localhost:4433");
-  console.log("Or use \"ssh root@localhost -p 2222\"");
-  
+  console.log('Or use "ssh root@localhost -p 2222"');
+
   spawnSync("qemu-kvm", [
     "-m",
     "2G",
@@ -39,6 +53,12 @@ try {
     // forward HTTPS (433) to local port 4433
     // forward SSH (22) to local port 2222
     "user,hostfwd=tcp::4433-:443,hostfwd=tcp::2222-:22",
+    "-kernel",
+    linux,
+    "-initrd",
+    initrd,
+    "-append",
+    `live.password=${password}`,
     "-cdrom",
     options.iso,
   ]);
